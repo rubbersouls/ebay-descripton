de714e93966cd6ce5f507f84f3bcb166ec3d8c1e
rubbersouls <98554097+rubbersouls@users.noreply.github.com>
2023-02-21 13:09:50 +0100

    Update script_ebay.js

--- script_ebay.js
+++ script_ebay.js
@@ -1,11 +1,9 @@
-    // Récupérer tous les boutons et toutes les descriptions
-    const buttons = document.querySelectorAll('.btn');
-    const descriptions = document.querySelectorAll('.description');
+const buttons = document.querySelectorAll('.btn');
+const descriptions = document.querySelectorAll('.description');
 
-    // Ajouter un écouteur d'événements à chaque bouton
+
     buttons.forEach((button, index) => {
       button.addEventListener('click', () => {
-        // Masquer toutes les descriptions
         descriptions.forEach((description) => {
           description.style.display = 'none';
         });
